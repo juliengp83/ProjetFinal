6a9a6eeb50e4deac458ab40f80f2d6c834c384d6
EricMarquisQuebec <9171475@crosemont.qc.ca>
2022-08-28 09:45:33 -0400

    Ajout de la règle 1

--- projetfinal/src/main/java/com/groupe1/utils/TraitementFeuille.java
+++ projetfinal/src/main/java/com/groupe1/utils/TraitementFeuille.java
@@ -12,7 +12,7 @@ public class TraitementFeuille {
     public static Resultat traitement(ArrayList<EmployeProjet> employe_projets) {
         Resultat res = new Resultat();
 
-        Regle r = testRegle1();
+        Regle r = testRegle1(employe_projets);
         //res.ajouterRegle(r);
         r = testRegle2(employe_projets);
         //res.ajouterRegle(r);
@@ -32,14 +32,35 @@ public class TraitementFeuille {
         return res;
     }
     
-    public static Regle testRegle1() {
-        Regle regle;
-
-        // Test #1 : Les employes de l'administration (numero_employe est INFERIEUR à 1000) doivent travailler AU MOINS 
-        // 36 heures au bureau par semaine (code_projet INFERIEUR à 900).
+    public static Regle testRegle1(ArrayList<EmployeProjet> employe_projets) {
+        // Test #1 : Les employes de l'administration (numero_employe est INFERIEUR à 1000) 
+        // doivent travailler AU MOINS 36 heures au bureau par semaine (code_projet INFERIEUR à 900).
         // faire les test
+
+        Regle regle;
+        boolean respectee = true;
+        String message = "L'employé administratif a travaillé au moins 36 heures au bureau cette semaine.";
+        int minutes_travaillees_bureau = 0;
+        int temps_requis_en_minutes = 60 * 36;
+        int numero_employe = employe_projets.get(0).getNumeroEmploye();
+
+        if (numero_employe > 1000) {
+            message = "Il s'agit d'un employé normal qui n'est assujeti par le règle 1.";
+        }
+        else {
+            for (EmployeProjet emp_p : employe_projets) {
+                if (emp_p.getNumeroProjet() < 900) {
+                    minutes_travaillees_bureau += emp_p.getTempsTravail();
+                }
+            }
+
+            if (minutes_travaillees_bureau < temps_requis_en_minutes) {
+                message = "L'employé administratif n'a pas travaillé au moins 36 heures au bureau cette semaine.";
+                respectee = false;
+            }
+        }
         
-        regle = new Regle(1, true, "le message");
+        regle = new Regle(1, respectee, message);
         return regle;
     }
 
