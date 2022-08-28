411cdd02f54a2c3d09e39d338132bcf49ae447d0
EricMarquisQuebec <9171475@crosemont.qc.ca>
2022-08-28 10:12:34 -0400

    Ajout de règles 1, 3, 4, 5

--- projetfinal/src/main/java/com/groupe1/modeles/EmployeProjet.java
+++ projetfinal/src/main/java/com/groupe1/modeles/EmployeProjet.java
@@ -1,6 +1,10 @@
 package com.groupe1.modeles;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalField;
+import java.time.temporal.WeekFields;
 import java.util.Date;
 import java.util.Locale;
 
@@ -25,27 +29,27 @@ public class EmployeProjet {
         this.date_travail = date_travail;
     }
 
-    public int getId_employe_projet() {
+    public int getIdEmployeProjet() {
         return id_employe_projet;
     }
 
-    public void setId_employeprojet(int id_employe_projet) {
+    public void setIdEmployeProjet(int id_employe_projet) {
         this.id_employe_projet = id_employe_projet;
     }
 
-    public int getNumero_employe() {
+    public int getNumeroEmploye() {
         return numero_employe;
     }
 
-    public void setNumero_employe(int numero_employe) {
+    public void setNumeroEmploye(int numero_employe) {
         this.numero_employe = numero_employe;
     }
 
-    public int getNumero_projet() {
+    public int getNumeroProjet() {
         return numero_projet;
     }
 
-    public void setNumero_projet(int numero_projet) {
+    public void setNumeroProjet(int numero_projet) {
         this.numero_projet = numero_projet;
     }
 
@@ -53,32 +57,41 @@ public class EmployeProjet {
         return id_feuille_temps;
     }
 
-    public void setId_feuille_temps(int id_feuille_temps) {
+    public void setIdFeuilleTemps(int id_feuille_temps) {
         this.id_feuille_temps = id_feuille_temps;
     }
 
-    public int getTemps_travail() {
+    public int getTempsTravail() {
         return temps_travaille;
     }
 
-    public void setTemps_travail(int temps_travail) {
+    public void setTempsTravail(int temps_travail) {
         this.temps_travaille = temps_travail;
     }
 
-    public String getDate_travail() {
+    public String getDateTravail() {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MMMMM-dd", Locale.CANADA_FRENCH);
 
         String date = simpleDateFormat.format(date_travail);
         return date;
     }
 
-    public void setDate_travail(Date date_travail) {
+    public void setDateTravail(Date date_travail) {
         this.date_travail = date_travail;
     }
-
+    
+    public int getWeekNum() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MMMMM-dd");
+        LocalDate date = LocalDate.parse(getDateTravail(), formatter);
+        WeekFields wf = WeekFields.of(Locale.getDefault());
+        TemporalField weekNum = wf.weekOfWeekBasedYear();
+        int week = Integer.parseInt(String.format("%02d", date.get(weekNum)));
+    
+        return week;
+    }
     @Override
     public String toString() {
-        return "EmployeProjet [date_travail=" + getDate_travail() + ", id_employe_projet=" + id_employe_projet
+        return "EmployeProjet [date_travail=" + getDateTravail() + ", id_employe_projet=" + id_employe_projet
                 + ", id_feuille_temps=" + id_feuille_temps + ", numero_employe=" + numero_employe + ", numero_projet="
                 + numero_projet + ", temps_travail=" + temps_travaille + "]";
     }
--- projetfinal/src/main/java/com/groupe1/utils/TraitementFeuille.java
+++ projetfinal/src/main/java/com/groupe1/utils/TraitementFeuille.java
@@ -9,90 +9,180 @@ import com.groupe1.modeles.Regle;
 import com.groupe1.modeles.Resultat;
 
 public class TraitementFeuille {
-
-    
     public static Resultat traitement(ArrayList<EmployeProjet> employe_projets) {
         Resultat res = new Resultat();
 
-        Regle r = testRegle1();
-        res.ajouterRegle(r);
-        r = testRegle2();
-        res.ajouterRegle(r);
-        r = testRegle3();
-        res.ajouterRegle(r);
+        Regle r = testRegle1(employe_projets);
+        // res.ajouterRegle(r);
+        r = testRegle2(employe_projets);
+        // res.ajouterRegle(r);
+        r = testRegle3(employe_projets);
+        // res.ajouterRegle(r);
         r = testRegle4();
-        res.ajouterRegle(r);
+        // res.ajouterRegle(r);
         r = testRegle5();
-        res.ajouterRegle(r);       
+        // res.ajouterRegle(r);
         r = testRegle6();
-        res.ajouterRegle(r);       
+        // res.ajouterRegle(r);
         r = testRegle7();
-        res.ajouterRegle(r);       
-        
+        // res.ajouterRegle(r);
+
         return res;
     }
-    
-    public static Regle testRegle1() {
-        Regle regle;
 
-        // Test #1 : Les employes de l'administration (numero_employe est INFERIEUR à 1000) doivent travailler AU MOINS 
-        // 36 heures au bureau par semaine (code_projet INFERIEUR à 900).
+    public static Regle testRegle1(ArrayList<EmployeProjet> employe_projets) {
+        // Test #1 : Les employes de l'administration (numero_employe est INFERIEUR à
+        // 1000)
+        // doivent travailler AU MOINS 36 heures au bureau par semaine (code_projet
+        // INFERIEUR à 900).
         // faire les test
-        
-        regle = new Regle(1, true, "le message");
+
+        Regle regle;
+        boolean respectee = true;
+        String message = "L'employé administratif a travaillé au moins 36 heures au bureau cette semaine.";
+        int minutes_travaillees_bureau = 0;
+        int temps_requis_en_minutes = 60 * 36;
+        int numero_employe = employe_projets.get(0).getNumeroEmploye();
+
+        if (numero_employe > 1000) {
+            message = "Il s'agit d'un employé normal qui n'est pas assujetti a cette règle.";
+        } else {
+            for (EmployeProjet emp_p : employe_projets) {
+                if (emp_p.getNumeroProjet() < 900) {
+                    minutes_travaillees_bureau += emp_p.getTempsTravail();
+                }
+            }
+
+            if (minutes_travaillees_bureau < temps_requis_en_minutes) {
+                message = "L'employé administratif n'a pas travaillé au moins 36 heures au bureau cette semaine.";
+                respectee = false;
+            }
+        }
+
+        regle = new Regle(1, respectee, message);
         return regle;
     }
 
-    public static Regle testRegle2() {
-        //Test #2 : Les employes normaux (numero_employe est SUPERIEUR à 1000) doivent travailler AU MOINS 38 heures au
-        //bureau par semaine (code_projet inferieur à 900).
-   
-        Regle regle = new Regle();
+    // Méthode testée manuellement, fonctionne correctement
+    public static Regle testRegle2(ArrayList<EmployeProjet> employe_projets) {
+        // Test #2 : Les employes normaux (numero_employe est SUPERIEUR à 1000) doivent
+        // travailler AU MOINS 38 heures au
+        // bureau par semaine (code_projet inferieur à 900).
+
+        Regle regle;
+        String message;
+        int minutes_travaillees_bureau = 0;
+        int temps_requis_en_minutes = 60 * 38;
+        int numero_employe = employe_projets.get(0).getNumeroEmploye();
+        if (numero_employe < 1000) {
+            message = "Il s'agit d'un employé de l'administration donc il remplit forcément cette règle";
+            regle = new Regle(2, true, message);
+        } else {
+            for (EmployeProjet emp_p : employe_projets) {
+                if (emp_p.getNumeroProjet() < 900)
+                    minutes_travaillees_bureau += emp_p.getTempsTravail();
+            }
+            if (minutes_travaillees_bureau < temps_requis_en_minutes) {
+                message = "L'employé régulier n'a pas travaillé au moins 38 heures au bureau cette semaine.";
+                regle = new Regle(2, false, message);
+            } else {
+                message = "L'employé régulier a travaillé au moins 38 heures au bureau cette semaine.";
+                regle = new Regle(2, true, message);
+            }
+        }
 
         return regle;
     }
 
-    public static Regle testRegle3() {
-        //Test #3 : Aucun employé n'a le droit de passer plus de 43 heures au bureau (code_projet inferieur à 900).
-        
-        Regle regle = new Regle();
-        
+    public static Regle testRegle3(ArrayList<EmployeProjet> employe_projets) {
+        // Test #3 : Aucun employé n'a le droit de passer plus de 43 heures au bureau
+        // (code_projet inferieur à 900).
+
+        Regle regle;
+        boolean respectee = true;
+        String message = "L'employé a travaillé moins 43 heures au bureau cette semaine.";
+        int minutes_travaillees_bureau = 0;
+        int temps_maximum_en_minutes = 60 * 43;
+
+        for (EmployeProjet emp_p : employe_projets) {
+            if (emp_p.getNumeroProjet() < 900) {
+                minutes_travaillees_bureau += emp_p.getTempsTravail();
+            }
+        }
+
+        if (minutes_travaillees_bureau > temps_maximum_en_minutes) {
+            message = "L'employé a travaillé plus de 43 heures au bureau cette semaine.";
+            respectee = false;
+        }
+
+        regle = new Regle(3, respectee, message);
+
         return regle;
     }
 
-    public static Regle testRegle4() {
-        //Test #4 : Les employés de l'administration (numero_employe est INFERIEUR a 1000) ne peuvent pas faire plus de
-        //10 heures de télétravail (code_projet SUPÉRIEUR à 1000) par semaine.
+    public static Regle testRegle4(ArrayList<EmployeProjet> employe_projets) {
+        // Test #4 : Les employés de l'administration (numero_employe est INFERIEUR a
+        // 1000) ne peuvent pas faire plus de
+        // 10 heures de télétravail (code_projet SUPÉRIEUR à 1000) par semaine.
+        Regle regle;
+        boolean respectee = true;
+        String message = "L'employé administratif a travaillé moins 10 heures en télétravail cette semaine.";
+        int minutes_travaillees_bureau = 0;
+        int temps_maximum_en_minutes = 60 * 10;
+        int numero_employe = employe_projets.get(0).getNumeroEmploye();
 
-        Regle regle = new Regle();
+        if (numero_employe > 1000) {
+            message = "Il s'agit d'un employé normal qui n'est assujeti par le règle.";
+        } else {
+            for (EmployeProjet emp_p : employe_projets) {
+                if (emp_p.getNumeroProjet() > 1000) {
+                    minutes_travaillees_bureau += emp_p.getTempsTravail();
+                }
+            }
 
+            if (minutes_travaillees_bureau < temps_maximum_en_minutes) {
+                message = "L'employé administratif a travaillé plus de 10 heures en télétravail cette semaine.";
+                respectee = false;
+            }
+        }
+
+        regle = new Regle(4, respectee, message);
         return regle;
-    }   
+    }
 
-    public static Regle testRegle5() {
-        //   Test #5 : Les employés normaux (numero_employe est SUPERIEUR à 1000) peuvent faire autant de télétravail 
-        //(code_projet supérieur à 1000) qu'ils le souhaitent.
-   
-        Regle regle = new Regle();
+    public static Regle testRegle5(ArrayList<EmployeProjet> employe_projets) {
+        // Test #5 : Les employés normaux (numero_employe est SUPERIEUR à 1000) peuvent
+        // faire autant de télétravail
+        // (code_projet supérieur à 1000) qu'ils le souhaitent.
+        String message = "L'employé normaux peuvent faire autant de télétravail qu'il veulent.";
+        int numero_employe = employe_projets.get(0).getNumeroEmploye();
+
+        if (numero_employe < 1000) {
+            message = "Il s'agit d'un employé administratif qui n'est pas assujetti a cette règle.";
+        }
+
+        Regle regle = new Regle(5, true, message);
 
         return regle;
     }
 
     public static Regle testRegle6() {
-        //Test #6 : Les employés normaux (numero_employe superieur à 1000) doivent faire un minimum de 6 heures au 
-        //bureau (code_projet inferieur à 1000) les jours ouvrables.
-   
+        // Test #6 : Les employés normaux (numero_employe superieur à 1000) doivent
+        // faire un minimum de 6 heures au
+        // bureau (code_projet inferieur à 1000) les jours ouvrables.
+
         Regle regle = new Regle();
 
         return regle;
     }
 
     public static Regle testRegle7() {
-        //Test #7 : Les employés de l'administration (numero_employe inferieur à 1000) doivent faire un minimum de 4 heures
-        //au bureau (code_projet inferieur à 1000) les jours ouvrables.
-        
+        // Test #7 : Les employés de l'administration (numero_employe inferieur à 1000)
+        // doivent faire un minimum de 4 heures
+        // au bureau (code_projet inferieur à 1000) les jours ouvrables.
+
         Regle regle = new Regle();
 
-        return regle; 
+        return regle;
     }
 }
