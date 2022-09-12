c3f98869f3454573fa1f332906bd51e4060a0295
dangrenier83 <dangrenier83@protonmail.com>
2022-09-12 12:57:00 -0400

    Migration vers spring boot de base

---
+++ projetfinal/src/main/java/com/groupe1/feuilletemps/FeuilletempsApplication.java
@@ -0,0 +1,13 @@
+package com.groupe1.feuilletemps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeuilletempsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeuilletempsApplication.class, args);
+	}
+
+}
---
+++ projetfinal/src/main/java/com/groupe1/feuilletemps/utils/LecteurJson.java
@@ -0,0 +1,39 @@
+package com.groupe1.feuilletemps.utils;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.groupe1.feuilletemps.modeles.EmployeProjet;
+
+public class LecteurJson {
+
+    /** Cette fonction permet la lecture d'un fichier JSON représentant la feuille de temps hebdomadaire complète
+     *  d'un employé.
+     * 
+     * @param nom_fichier Le nom complet du fichier JSON contenant la feuille de temps dans le format structuré
+     * demandé 
+     * @return Une ArrayList<EmployeProjet> représentant les données entrées par l'employé pour la semaine entière
+     */
+    public static ArrayList<EmployeProjet> lireFichier(String nom_fichier){
+        Gson gson = new Gson();
+
+        ArrayList<EmployeProjet> employe_projets = new ArrayList<>();
+        try {
+            Reader lecteur_fichier = Files.newBufferedReader(Paths.get(nom_fichier));
+            
+            employe_projets = gson.fromJson(lecteur_fichier, new TypeToken<List<EmployeProjet>>() {}.getType());
+
+        } catch (IOException ex) {
+            System.out.println("Une erreur est survenue, le fichier d'entrée que vous avez entré n'existe pas");
+            System.exit(-1);
+        }
+
+        return employe_projets;
+    }
+}
