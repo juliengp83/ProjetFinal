1b6eeb87b39f02c46fa0a9d5db1be91ab2fc06dc
Emile Tremblay <emiletremblay01@gmail.com>
2022-09-25 22:02:01 -0400

    ajout interaction avec database gestionscript.js

--- projetfinal/src/main/java/com/groupe1/feuilletemps/FeuilletempsApplication.java
+++ projetfinal/src/main/java/com/groupe1/feuilletemps/FeuilletempsApplication.java
@@ -1,7 +1,15 @@
 package com.groupe1.feuilletemps;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.groupe1.feuilletemps.data.EmployeRepository;
+import com.groupe1.feuilletemps.data.ProjetRepository;
+import com.groupe1.feuilletemps.modeles.Employe;
+import com.groupe1.feuilletemps.modeles.Projet;
+import com.groupe1.feuilletemps.utils.AES;
 
 @SpringBootApplication
 public class FeuilletempsApplication {
@@ -10,93 +18,98 @@ public class FeuilletempsApplication {
 		SpringApplication.run(FeuilletempsApplication.class, args);
 	}
 
-	/* 	
-	@Bean
-	public CommandLineRunner data_loader(EmployeRepository repo_emp, ProjetRepository repo_proj) {
-		return new CommandLineRunner() {
-			@Override
-			public void run(String... args) throws Exception {
-				AES.setKey("bBgLrINTjBINrm7");
+	// @Bean
+	// public CommandLineRunner data_loader(EmployeRepository repo_emp,
+	// ProjetRepository repo_proj) {
+	// return new CommandLineRunner() {
+	// @Override
+	// public void run(String... args) throws Exception {
+	// AES.setKey("bBgLrINTjBINrm7");
 
-				Projet projet1 = new Projet(1000L, "Télétravail");
-				Projet projet2 = new Projet(500L, "Bureau");
-				Projet projet3 = new Projet(100L, "Bureau2");
+	// Projet projet1 = new Projet(1000L, "Télétravail");
+	// Projet projet2 = new Projet(500L, "Bureau");
+	// Projet projet3 = new Projet(100L, "Bureau2");
 
-				Projet p995 = new Projet(995L, "Congé parental");
-				Projet p996 = new Projet(996L, "Transport en voiture");
-				Projet p997 = new Projet(997L, "Transport en commun");
-				Projet p998 = new Projet(998L, "Congé férié");
-				Projet p999 = new Projet(999L, "Congé de maladie");
+	// Projet p995 = new Projet(995L, "Congé parental");
+	// Projet p996 = new Projet(996L, "Transport en voiture");
+	// Projet p997 = new Projet(997L, "Transport en commun");
+	// Projet p998 = new Projet(998L, "Congé férié");
+	// Projet p999 = new Projet(999L, "Congé de maladie");
 
-				repo_proj.save(projet1);
-				repo_proj.save(projet2);
-				repo_proj.save(projet3);
-				repo_proj.save(p995);
-				repo_proj.save(p996);
-				repo_proj.save(p997);
-				repo_proj.save(p998);
-				repo_proj.save(p999);
+	// repo_proj.save(projet1);
+	// repo_proj.save(projet2);
+	// repo_proj.save(projet3);
+	// repo_proj.save(p995);
+	// repo_proj.save(p996);
+	// repo_proj.save(p997);
+	// repo_proj.save(p998);
+	// repo_proj.save(p999);
 
-				Employe employe1 = new Employe(1L, "Hamdael", "Steelbeard", "hamdael@julexmile.com",
-					"hamdael", AES.encrypt("steelbeard123", "bBgLrINTjBINrm7"));
-				Employe employe2 = new Employe(1112L, "Thralmig", "Deepbrew", "thralmig@julexmile.com",
-					"thralmig", AES.encrypt("thralmig123", "bBgLrINTjBINrm7"));
-				Employe employe3 = new Employe(3L, "Mundaeth", "Dragonbender", "Mundaeth",
-					"mundaeth",	AES.encrypt("mundaeth123", "bBgLrINTjBINrm7"));
-				Employe employe4 = new Employe(4L, "Thromdol", "Hillbreaker", "thromdol@julexmile.com",
-				    "thromdol", AES.encrypt("thromdol", "bBgLrINTjBINrm7"));
-				Employe employe5 = new Employe(5L, "Valgid", "Honordigger", "valgid@julexmile.com", 
-					"valgid", AES.encrypt("valgid123", "bBgLrINTjBINrm7"));				
-					
-				employe1.ajouterProjet(projet3);
-				employe1.ajouterProjet(projet1);
+	// Employe employe1 = new Employe(1L, "Hamdael", "Steelbeard",
+	// "hamdael@julexmile.com",
+	// "hamdael", AES.encrypt("steelbeard123", "bBgLrINTjBINrm7"));
+	// Employe employe2 = new Employe(1112L, "Thralmig", "Deepbrew",
+	// "thralmig@julexmile.com",
+	// "thralmig", AES.encrypt("thralmig123", "bBgLrINTjBINrm7"));
+	// Employe employe3 = new Employe(3L, "Mundaeth", "Dragonbender", "Mundaeth",
+	// "mundaeth", AES.encrypt("mundaeth123", "bBgLrINTjBINrm7"));
+	// Employe employe4 = new Employe(4L, "Thromdol", "Hillbreaker",
+	// "thromdol@julexmile.com",
+	// "thromdol", AES.encrypt("thromdol", "bBgLrINTjBINrm7"));
+	// Employe employe5 = new Employe(5L, "Valgid", "Honordigger",
+	// "valgid@julexmile.com",
+	// "valgid", AES.encrypt("valgid123", "bBgLrINTjBINrm7"));
 
-				employe2.ajouterProjet(projet1);
-				employe2.ajouterProjet(projet3);
-				employe2.ajouterProjet(p995);
-				employe2.ajouterProjet(p996);
-				employe2.ajouterProjet(p997);
-				employe2.ajouterProjet(p998);
-				employe2.ajouterProjet(p999);
-			
-				employe3.ajouterProjet(projet1);
-				employe3.ajouterProjet(projet2);
-				employe3.ajouterProjet(projet3);
-				employe3.ajouterProjet(p995);
-				employe3.ajouterProjet(p996);
-				employe3.ajouterProjet(p997);
-				employe3.ajouterProjet(p998);
-				employe3.ajouterProjet(p999);
+	// employe1.ajouterProjet(projet3);
+	// employe1.ajouterProjet(projet1);
 
-				employe4.ajouterProjet(projet3);
-				employe4.ajouterProjet(projet1);
+	// employe2.ajouterProjet(projet1);
+	// employe2.ajouterProjet(projet3);
+	// employe2.ajouterProjet(p995);
+	// employe2.ajouterProjet(p996);
+	// employe2.ajouterProjet(p997);
+	// employe2.ajouterProjet(p998);
+	// employe2.ajouterProjet(p999);
 
-				employe5.ajouterProjet(projet1);
-				employe5.ajouterProjet(projet3);
-			
-				repo_emp.save(employe1);
-				repo_emp.save(employe2);
-				repo_emp.save(employe3);
-				repo_emp.save(employe4);
-				repo_emp.save(employe5);
+	// employe3.ajouterProjet(projet1);
+	// employe3.ajouterProjet(projet2);
+	// employe3.ajouterProjet(projet3);
+	// employe3.ajouterProjet(p995);
+	// employe3.ajouterProjet(p996);
+	// employe3.ajouterProjet(p997);
+	// employe3.ajouterProjet(p998);
+	// employe3.ajouterProjet(p999);
 
-				// ajout d'un employe admin et regulier juste pour tester rapidement
-				Employe admin = new Employe(1000L, "Emile", "Tremblay", "2020 Avenue", "admin",
-						AES.encrypt("", "bBgLrINTjBINrm7"));
-				admin.ajouterProjet(projet1);
-				admin.ajouterProjet(projet2);
-				admin.ajouterProjet(projet3);
-				repo_emp.save(admin);
-				Employe regulier = new Employe(1001L, "Emile", "Tremblay", "3030 Avenue", "reg",
-						AES.encrypt("", "bBgLrINTjBINrm7"));
-				regulier.ajouterProjet(projet1);
-				regulier.ajouterProjet(projet2);
-				regulier.ajouterProjet(projet3);
-				repo_emp.save(regulier);
-			}
-			
-		};
-	}
-	 */
+	// employe4.ajouterProjet(projet3);
+	// employe4.ajouterProjet(projet1);
+
+	// employe5.ajouterProjet(projet1);
+	// employe5.ajouterProjet(projet3);
+
+	// repo_emp.save(employe1);
+	// repo_emp.save(employe2);
+	// repo_emp.save(employe3);
+	// repo_emp.save(employe4);
+	// repo_emp.save(employe5);
+
+	// // ajout d'un employe admin et regulier juste pour tester rapidement
+	// Employe admin = new Employe(1000L, "Emile", "Tremblay", "2020 Avenue",
+	// "admin",
+	// AES.encrypt("", "bBgLrINTjBINrm7"));
+	// admin.ajouterProjet(projet1);
+	// admin.ajouterProjet(projet2);
+	// admin.ajouterProjet(projet3);
+	// repo_emp.save(admin);
+	// Employe regulier = new Employe(1001L, "Emile", "Tremblay", "3030 Avenue",
+	// "reg",
+	// AES.encrypt("", "bBgLrINTjBINrm7"));
+	// regulier.ajouterProjet(projet1);
+	// regulier.ajouterProjet(projet2);
+	// regulier.ajouterProjet(projet3);
+	// repo_emp.save(regulier);
+	// }
+
+	// };
+	// }
 
 }
--- projetfinal/src/main/resources/static/scripts/gestionscript.js
+++ projetfinal/src/main/resources/static/scripts/gestionscript.js
@@ -1,86 +1,226 @@
-// $('#search-select')
-//   .dropdown();
+$(document).ready(function () {
+  // global arrays!!!
+  const employes = JSON.parse($("#employes").text());
+  const formulaires = [];
 
-$(".ui.dropdown").dropdown({
-  action: "activate",
-  onChange: function (value, text, $selectedItem) {
-    $.ajax({
-      type: "GET",
-      url: "getfeuilles?selectedWeek=" + value,
-      //data: value,
-      success: function (x) {
-        console.log(x);
-      },
+  $(".ui.dropdown").dropdown({
+    action: "activate",
+    onChange: function (value, text, $selectedItem) {
+      $.ajax({
+        type: "GET",
+        url: "getfeuilles?selectedWeek=" + value,
+        success: function (f) {
+          const feuilles_de_temps = JSON.parse(f);
+          formulaires.length = 0;
+          for (const i of feuilles_de_temps) {
+            formulaires.push(i);
+          }
+          appendCards(formulaires);
+        },
+      });
+    },
+  });
+
+  $("#table-container").hide();
+
+  // -----------------------------ON_CLICK----------------------------------
+  $(".ui.menu > .item").click(function (e) {
+    e.preventDefault();
+    $(".ui.menu > .item").removeClass("active");
+    $(this).addClass("active");
+    showCards($(this).attr("id"));
+  });
+
+  $("#table-container")
+    .find(".close.icon")
+    .click(function (e) {
+      e.preventDefault();
+      $(this).closest("#table-container").hide();
     });
-  },
-});
-$("#table-container").hide();
-// $("#table-approved").hide();
-// $("#table-unapproved").hide();
-// $("#table-unapproved-errors").hide();
-// $("#table-unsent").hide();
 
-$(".ui.menu > .item").click(function (e) {
-  e.preventDefault();
-  $(".ui.menu > .item").removeClass("active");
-  $(this).addClass("active");
-  showCards($(this).attr("id"));
-});
-
-// -----------------------------ON_CLICK----------------------------------
-$("#table-container")
-  .find(".close.icon")
-  .click(function (e) {
-    e.preventDefault();
-    $(this).closest("#table-container").hide();
-  });
-
-$(".ui.segment")
-  .find(".ui.card")
-  .click(function (e) {
-    e.preventDefault();
-    showTable($(this).attr("class"));
-  });
-
-// -----------------------------CUSTOM_METHODS----------------------------------
-
-function showCards(menu_id) {
-  switch (menu_id) {
-    case "menu-all":
-      $(".ui.card").show();
-      break;
-    case "menu-approved":
-      $(".ui.card").hide();
-      $(".ui.card.card-approved").show();
-      break;
-    case "menu-unapproved":
-      $(".ui.card").hide();
-      $(".ui.card.card-unapproved").show();
-      $(".ui.card.card-errors-unapproved").show();
-      break;
-    case "menu-unsent":
-      $(".ui.card").hide();
-      $(".ui.card.card-unsent").show();
-      break;
-    default:
-      break;
+  function refreshCardClick() {
+    $(".ui.segment")
+      .find(".ui.card")
+      .click(function (e) {
+        e.preventDefault();
+        let emp = getEmployeFromCard($(this).find(".meta").first().text());
+        showTable($(this).attr("class"), emp);
+      });
   }
-}
+  // -----------------------------CUSTOM_METHODS----------------------------------
 
-function showTable(str_classes) {
-  // debugger;
-  $("#table-container > div").hide();
-  if (str_classes.match("card-approved") != null) {
-    $("#table-container").show();
-    $("#table-approved").show();
-  } else if (str_classes.match("card-unapproved") != null) {
-    $("#table-container").show();
-    $("#table-unapproved").show();
-  } else if (str_classes.match("card-errors-unapproved") != null) {
-    $("#table-container").show();
-    $("#table-unapproved-errors").show();
-  } else if (str_classes.match("card-unsent") != null) {
-    $("#table-container").show();
-    $("#table-unsent").show();
+  function showCards(menu_id) {
+    switch (menu_id) {
+      case "menu-all":
+        $(".ui.card").show();
+        break;
+      case "menu-approved":
+        $(".ui.card").hide();
+        $(".ui.card.card-approved").show();
+        break;
+      case "menu-unapproved":
+        $(".ui.card").hide();
+        $(".ui.card.card-unapproved").show();
+        $(".ui.card.card-errors-unapproved").show();
+        break;
+      case "menu-unsent":
+        $(".ui.card").hide();
+        $(".ui.card.card-unsent").show();
+        break;
+      default:
+        break;
+    }
   }
-}
+
+  function showTable(str_classes, employe) {
+    $("#table-container > div").hide();
+    if (str_classes.match("card-approved") != null) {
+      createTableProjets(employe, "#table-approved");
+      $("#table-container").show();
+      $("#table-approved").show();
+    } else if (str_classes.match("card-unapproved") != null) {
+      createTableProjets(employe, "#table-unapproved");
+      $("#table-container").show();
+      $("#table-unapproved").show();
+    } else if (str_classes.match("card-errors-unapproved") != null) {
+      createTableProjets(employe, "#table-unapproved-errors");
+      $("#table-container").show();
+      $("#table-unapproved-errors").show();
+    } else if (str_classes.match("card-unsent") != null) {
+      createTableUnsent(employe);
+      $("#table-container").show();
+      $("#table-unsent").show();
+    }
+  }
+
+  // cards creation
+  function appendCards(feuilles_de_temps) {
+    $(".ui.cards").empty();
+    for (const employe of employes) {
+      let isAvailable = false;
+      let obj_feuille = null;
+      for (const feuille of feuilles_de_temps) {
+        if (employe.employeId == feuille.employe.employeId) {
+          isAvailable = true;
+          obj_feuille = feuille;
+          break;
+        }
+      }
+      if (isAvailable) {
+        if (obj_feuille.estApprouve) {
+          createCardApproved(employe);
+        } else {
+          createCardUnapproved(employe, obj_feuille.estValidee);
+        }
+      } else {
+        createCardUnsent(employe);
+      }
+    }
+    refreshCardClick();
+  }
+
+  function createCardUnsent(employe) {
+    const card_string_start =
+      '<a class="ui grey card card-unsent"><div class="content">';
+    const header =
+      '<div class="header">' + employe.prenom + " " + employe.nom + "</div>";
+    const id = '<div class="meta">ID:' + employe.employeId + "</div>";
+    const card_string_end =
+      '</div><div class="extra content"><i class="ellipsis horizontal icon"></i>En attente du formulaire</div></a>';
+    const card_string_final = card_string_start + header + id + card_string_end;
+    $(".ui.cards").append(card_string_final);
+  }
+
+  function createCardUnapproved(employe, estValidee) {
+    let card_string_start = "";
+    if (estValidee) {
+      card_string_start =
+        '<a class="ui orange card card-unapproved"><div class="content">';
+    } else {
+      card_string_start =
+        '<a class="ui red card card-errors-unapproved"><div class="content">';
+    }
+    const header =
+      '<div class="header">' + employe.prenom + " " + employe.nom + "</div>";
+    const id = '<div class="meta">ID:' + employe.employeId + "</div>";
+    const card_string_end =
+      '</div><div class="extra content"><i class="exclamation icon"></i>Formulaire à approuver</div></a>';
+    const card_string_final = card_string_start + header + id + card_string_end;
+    $(".ui.cards").append(card_string_final);
+  }
+
+  function createCardApproved(employe) {
+    const card_string_start =
+      '<a class="ui green card card-approved"><div class="content">';
+    const header =
+      '<div class="header">' + employe.prenom + " " + employe.nom + "</div>";
+    const id = '<div class="meta">ID:' + employe.employeId + "</div>";
+    const card_string_end =
+      '</div><div class="extra content"><i class="check icon"></i>Formulaire approuvé</div></a>';
+    const card_string_final = card_string_start + header + id + card_string_end;
+    $(".ui.cards").append(card_string_final);
+  }
+
+  // table creation
+  function createTableUnsent(employe) {
+    $("#table-unsent").find(".message").empty();
+    const start_str =
+      "<h4>" +
+      employe.prenom +
+      " n'a pas encore rempli son formulaire pour cette semaine.</h4>";
+    const end_str =
+      "<h4>Contactez " +
+      employe.prenom +
+      ' via <a href="mailto:">' +
+      employe.courriel +
+      "</a> pour lui demander de remplir son formulaire.</h4>";
+    const final_str = start_str + end_str;
+    $("#table-unsent").find(".message").append(final_str);
+  }
+
+  function getEmployeFromCard(meta_txt) {
+    let id = parseInt(meta_txt.substr(3));
+    let employe = null;
+    for (const emp of employes) {
+      if (emp.employeId == id) {
+        employe = emp;
+        break;
+      }
+    }
+    return employe;
+  }
+
+  function createTableProjets(employe, table_id) {
+    let formulaire = null;
+    for (const f of formulaires) {
+      if (f.employe.employeId == employe.employeId) {
+        formulaire = f;
+        break;
+      }
+    }
+    $(table_id).find("tbody").empty();
+    for (const entree of formulaire.entreesProjets) {
+      const nom_projet =
+        '<td data-label="nom_projet">' + entree.projet.nom_projet + "</td>";
+      const temps =
+        '<td data-label="tempsTravaille">' + entree.tempsTravaille + "</td>";
+      const date =
+        '<td data-label="dateTravaille">' + entree.dateTravaille + "</td>";
+      const final = "<tr>" + nom_projet + temps + date + "</tr>";
+      $(table_id).find("tbody").append(final);
+    }
+    if (table_id === "#table-unapproved-errors") {
+      $(table_id).find(".message").empty();
+      const start_str =
+        "<h4>Une ou plusieurs règles ne sont pas respectées par ce formulaire.</h4>";
+      const end_str =
+        "<h4>Contactez " +
+        employe.prenom +
+        ' via <a href="mailto:">' +
+        employe.courriel +
+        "</a> avant d'approuver le formulaire.</h4>";
+      const final_str = start_str + end_str;
+      $(table_id).find(".message").append(final_str);
+    }
+  }
+});
